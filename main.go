652661007d83f905d1e49e7f58cb49dc19c61a40
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-09-21 23:24:39 +0200

    Refactor and add workflows

--- main.go
+++ main.go
@@ -12,86 +12,76 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-type Server struct {
+type server struct {
 	Online uint `json:"online"`
 	Max    uint `json:"max"`
 }
 
 var api string
+var servers []string
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
+func init() {
 	api = os.Getenv("API_URL")
 	if len(api) == 0 {
 		log.Panic("Empty api url")
 	}
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	b, err := refresh()
 	if err != nil {
 		log.Panic(err)
 	}
 
-	var servers []string
-	json.Unmarshal(b, &servers)
+	if err := json.Unmarshal(b, &servers); err != nil {
+		log.Panic(err)
+	}
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
+
 	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		if update.Message.IsCommand() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "status":
+		if !update.Message.IsCommand() {
+			continue
+		}
 
-				var str strings.Builder
-				for _, serv := range servers {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
-					resp, err := http.Get(api + "/serben/" + serv)
-					if err != nil {
-						str.WriteString(serv)
-						str.WriteString(": Error (")
-						str.WriteString(resp.Status)
-						str.WriteString(")\n")
-					} else if resp.StatusCode == http.StatusServiceUnavailable {
-						resp.Body.Close()
-						str.WriteString(serv)
-						str.WriteString(": Offline\n")
-					} else {
-						b, err = ioutil.ReadAll(resp.Body)
-						resp.Body.Close()
-						var s Server
-						json.Unmarshal(b, &s)
-						str.WriteString(serv)
-						str.WriteString(": ")
-						str.WriteString(strconv.FormatUint(uint64(s.Online), 10))
-						str.WriteString("/")
-						str.WriteString(strconv.FormatUint(uint64(s.Max), 10))
-						str.WriteString("\n")
-					}
-				}
-				msg.Text = str.String()
-			case "refresh":
-				b, err := refresh()
-				if err != nil {
-					msg.Text = "Unable to refresh"
-				} else {
-					json.Unmarshal(b, &servers)
+		switch update.Message.Command() {
+		case "status":
+			msg.Text = status()
+		case "refresh":
+			if b, err := refresh(); err == nil {
+				if err := json.Unmarshal(b, &servers); err == nil {
 					msg.Text = "Server: " + strings.Join(servers, ",")
+				} else {
+					msg.Text = "Unmarshal error"
 				}
-			default:
-				msg.Text = "I don't know that command"
+			} else {
+				msg.Text = "Unable to refresh"
 			}
-			bot.Send(msg)
+		default:
+			msg.Text = "I don't know that command"
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Printf("Error sending msg %s : %v", msg.Text, err)
 		}
 	}
 }
@@ -103,10 +93,52 @@ func refresh() ([]byte, error) {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
+	defer resp.Body.Close() // nolint: errcheck
+
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	return b, nil
+}
+
+func status() string {
+	var str strings.Builder
+
+	for _, serv := range servers {
+		resp, err := http.Get(api + "/serben/" + serv)
+		if err != nil {
+			str.WriteString(serv)
+			str.WriteString(": Error (")
+			str.WriteString(resp.Status)
+			str.WriteString(")\n")
+			continue
+		}
+
+		defer resp.Body.Close() // nolint: errcheck
+
+		if resp.StatusCode == http.StatusServiceUnavailable {
+			str.WriteString(serv)
+			str.WriteString(": Offline\n")
+			continue
+		}
+
+		b, _ := ioutil.ReadAll(resp.Body)
+
+		var s server
+		if err := json.Unmarshal(b, &s); err != nil {
+			str.WriteString(serv)
+			str.WriteString(": Unmarshal error")
+			continue
+		}
+
+		str.WriteString(serv)
+		str.WriteString(": ")
+		str.WriteString(strconv.FormatUint(uint64(s.Online), 10))
+		str.WriteString("/")
+		str.WriteString(strconv.FormatUint(uint64(s.Max), 10))
+		str.WriteString("\n")
+	}
+
+	return str.String()
 }
