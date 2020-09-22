13b3aad10bddbd40a034640bc5a160c4a909b822
Enduvar <70820452+Enduvar@users.noreply.github.com>
2020-09-22 23:08:17 +0200

    Refactor

--- main.go
+++ main.go
@@ -12,32 +12,35 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-type Server struct {
+type server struct {
 	Online uint `json:"online"`
 	Max    uint `json:"max"`
 }
 
 var api string
+var servers []string
 
-func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
-	if err != nil {
-		log.Panic(err)
-	}
+func init() {
 	api = os.Getenv("API_URL")
 	if len(api) == 0 {
 		log.Panic("Empty api url")
 	}
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	b, err := refresh()
 	if err != nil {
 		log.Panic(err)
 	}
 
-	var servers []string
 	json.Unmarshal(b, &servers)
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -52,51 +55,25 @@ func main() {
 			continue
 		}
 
-		if update.Message.IsCommand() {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "status":
-
-				var str strings.Builder
-				for _, serv := range servers {
-
-					resp, err := http.Get(api + "/serben/" + serv)
-					if err != nil {
-						str.WriteString(serv)
-						str.WriteString(": Error (")
-						str.WriteString(resp.Status)
-						str.WriteString(")\n")
-					} else if resp.StatusCode == http.StatusServiceUnavailable {
-						resp.Body.Close()
-						str.WriteString(serv)
-						str.WriteString(": Offline\n")
-					} else {
-						b, _ = ioutil.ReadAll(resp.Body)
-						resp.Body.Close()
-						var s Server
-						json.Unmarshal(b, &s)
-						str.WriteString(serv)
-						str.WriteString(": ")
-						str.WriteString(strconv.FormatUint(uint64(s.Online), 10))
-						str.WriteString("/")
-						str.WriteString(strconv.FormatUint(uint64(s.Max), 10))
-						str.WriteString("\n")
-					}
-				}
-				msg.Text = str.String()
-			case "refresh":
-				b, err := refresh()
-				if err != nil {
-					msg.Text = "Unable to refresh"
-				} else {
-					json.Unmarshal(b, &servers)
-					msg.Text = "Server: " + strings.Join(servers, ",")
-				}
-			default:
-				msg.Text = "I don't know that command"
-			}
-			bot.Send(msg)
+		if !update.Message.IsCommand() {
+			continue
 		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "status":
+			msg.Text = status()
+		case "refresh":
+			if b, err := refresh(); err == nil {
+				json.Unmarshal(b, &servers)
+				msg.Text = "Server: " + strings.Join(servers, ",")
+			} else {
+				msg.Text = "Unable to refresh"
+			}
+		default:
+			msg.Text = "I don't know that command"
+		}
+		bot.Send(msg)
 	}
 }
 
@@ -113,4 +90,39 @@ func refresh() ([]byte, error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+func status() string {
+	var str strings.Builder
+	for _, serv := range servers {
+		resp, err := http.Get(api + "/serben/" + serv)
+		if err != nil {
+			str.WriteString(serv)
+			str.WriteString(": Error (")
+			str.WriteString(resp.Status)
+			str.WriteString(")\n")
+			continue
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusServiceUnavailable {
+			str.WriteString(serv)
+			str.WriteString(": Offline\n")
+			continue
+
+		}
+
+		b, _ := ioutil.ReadAll(resp.Body)
+		var s server
+		json.Unmarshal(b, &s)
+		str.WriteString(serv)
+		str.WriteString(": ")
+		str.WriteString(strconv.FormatUint(uint64(s.Online), 10))
+		str.WriteString("/")
+		str.WriteString(strconv.FormatUint(uint64(s.Max), 10))
+		str.WriteString("\n")
+	}
+
+	return str.String()
 }
